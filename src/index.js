275132e0ed0620d0eb990e48ed1345af10704e14
Carlos Ramos González <emugameplay.tv@gmail.com>
2024-10-31 00:29:56 +0000

    bien

--- src/index.js
+++ src/index.js
@@ -11,7 +11,7 @@ dotenv.config();
 app.set('port', process.env.PORT || 5000);
 app.use(express.static(__dirname + '/public'))
 //middlewares
-app.use(cors());
+app.use(cors()); 
 app.use(morgan('dev'));
 app.use(express.json());
 

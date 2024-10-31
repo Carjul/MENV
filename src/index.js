5b5098eb8f302af77fa5846169e478bf7b51ce84
Carlos Ramos González <emugameplay.tv@gmail.com>
2024-10-31 00:52:27 +0000

    como antes

--- src/index.js
+++ src/index.js
@@ -1,7 +1,6 @@
 const express = require('express');
 const morgan = require('morgan')
 const http = require('http');
-const cors = require('cors'); 
 const app = express();
 const server = http.createServer(app);
 const dotenv = require('dotenv');
@@ -10,8 +9,8 @@ const dotenv = require('dotenv');
 dotenv.config();
 app.set('port', process.env.PORT || 5000);
 app.use(express.static(__dirname + '/public'))
+
 //middlewares
-app.use(cors()); 
 app.use(morgan('dev'));
 app.use(express.json());
 
